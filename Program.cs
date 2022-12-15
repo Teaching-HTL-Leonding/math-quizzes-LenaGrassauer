a477a0467436df51fc811478922692b34895836d
LenaGrassauer <116261857+LenaGrassauer@users.noreply.github.com>
2022-12-15 10:10:47 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿int number = 0;
+int reverse = 0;
+
+System.Console.Write("Please enter a number: ");
+number = int.Parse(Console.ReadLine()!);
+
+int außenrum = number;
+
+if (number < 10)
+{
+    System.Console.WriteLine("Your number is a Palindrome");
+}
+else
+{
+    do
+    {
+        
+    } while (number != 0);
+
+    if (außenrum == reverse)
+    {
+        Console.WriteLine("Your number is a Palindrome");
+    }
+    else 
+    {
+        Console.WriteLine("Your number is not a Palindrome");
+    }
+}
